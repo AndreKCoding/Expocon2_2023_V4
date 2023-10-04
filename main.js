0b796c9a925dcae9e7927c9d477ed958c7b6e6dc
AndreKCoding <131627515+AndreKCoding@users.noreply.github.com>
2023-10-04 16:18:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function G1() {
+    window.location = "Screens/G1.html";
+}
+
+function G2() {
+    window.location = "Screens/G2.html";
+}
+
+function G3() {
+    window.location = "Screens/G3.html";
+}
+
+function G4() {
+    window.location = "Screens/G4.html";
+}
+
+function feed() {
+    window.location = "https://forms.gle/GqxWbLY3UraN55ep9";
+}
+// https://forms.gle/GqxWbLY3UraN55ep9 Link do Google Forms
