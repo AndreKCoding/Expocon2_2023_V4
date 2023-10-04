f72f6c25716a50a55a6074013d8f6a3a141c31cf
AndreKCoding <131627515+AndreKCoding@users.noreply.github.com>
2023-10-04 16:17:50 -0300

    Add files via upload

---
+++ Screens/main.js
@@ -0,0 +1,7 @@
+function r() {
+    window.location = "../index.html";
+}
+
+function video() {
+    window.location = "https://drive.google.com/file/d/1xTiheGqtism5eoG0PI7YayJQYsHhVY_5/view?usp=sharing"
+}
